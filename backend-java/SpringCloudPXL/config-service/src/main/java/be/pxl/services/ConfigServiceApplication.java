cc516f8803d2faeba7b805194355061ebfd7e958
LotteJansenPXL <lotte.jansen@student.pxl.be>
2024-12-05 11:23:32 +0100

    Bugfix for config service

--- backend-java/SpringCloudPXL/config-service/src/main/java/be/pxl/services/ConfigServiceApplication.java
+++ backend-java/SpringCloudPXL/config-service/src/main/java/be/pxl/services/ConfigServiceApplication.java
@@ -3,6 +3,7 @@ package be.pxl.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Config Service
